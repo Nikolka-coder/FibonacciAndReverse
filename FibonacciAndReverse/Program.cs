89f64682ca1f4e3c0df96d67bafc61114fb17d1e
Veronika <Veronika@DESKTOP-S01858F>
2021-07-26 15:32:02 +0300

    Add project files.

---
+++ FibonacciAndReverse/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FibonacciAndReverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
